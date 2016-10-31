23d37452303387eb8c5b7ae1941d2d2c4be69416
siamaksc <siamaksc@kth.se>
2016-10-31 14:59:17 +0100

    commit from dev branch, I added 333 line to print

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
 	public static void main(String[] args){
 		System.out.println("This line I added from dev branch!");
 		System.out.println("This line was added from master branch");
+		System.out.println("This line I added from dev branch just for merging333");
 	}
-
 }
