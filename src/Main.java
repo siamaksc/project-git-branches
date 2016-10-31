5e7f3c3af51387d4effefe96b18850c9d194f218
siamaksc <siamaksc@kth.se>
2016-10-31 14:48:56 +0100

    Commit from master, I added second print

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
 	public static void main(String[] args){
 		System.out.println("This line I added from dev branch!");
+		System.out.println("This line was added from master branch");
 	}
 
 }
