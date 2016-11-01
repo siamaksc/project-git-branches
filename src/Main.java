ddcf010c2d7ce806a4ba7e01b598f3f6a53d65a0
siamaksc <siamaksc@kth.se>
2016-11-01 14:47:43 +0100

    I added the add method

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@ public class Main {
 	public static void main(String[] args){
 		System.out.println("print from raw 3");
 	}
+	
+	public double add(double firstNumber, double secondNumber){
+		return firstNumber + secondNumber;
+	}
 }
